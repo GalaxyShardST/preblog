ed10f8b7ea186cd525d4f638019a6f47fa90586b
GalaxyShard <dominic.adragna@byteroach.com>
2025-04-18 03:02:32 -0400

    use theme from prefers-color-scheme by default

--- dark-mode.js
+++ dark-mode.js
@@ -2,23 +2,44 @@
 
 // This script is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-let darkMode = localStorage.getItem("dark-mode");
-if (darkMode) {
+let defaultIsDark = false;
+
+if (window.matchMedia) {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        const isDark = event.matches;
+        defaultIsDark = isDark;
+        if (isDark) {
+            document.documentElement.classList.add("dark-mode");
+        } else {
+            document.documentElement.classList.remove("dark-mode");
+        }
+    });
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        defaultIsDark = true;
+        document.documentElement.classList.add("dark-mode");
+    }
+}
+
+const originalTheme = localStorage.getItem("theme");
+if (originalTheme == "dark") {
     document.documentElement.classList.add("dark-mode");
+} else if (originalTheme == "light") {
+    document.documentElement.classList.remove("dark-mode");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.getElementById("theme-switcher");
 
     button.addEventListener("click", () => {
-        darkMode = !darkMode;
+        document.documentElement.classList.toggle("dark-mode");
+        const isDark = document.documentElement.classList.contains("dark-mode");
 
-        if (darkMode) {
-            localStorage.setItem("dark-mode", true);
-            document.documentElement.classList.add("dark-mode");
+        if ((defaultIsDark && isDark) || (!defaultIsDark && !isDark)) {
+            localStorage.removeItem("theme");
+        } else if (isDark) {
+            localStorage.setItem("theme", "dark");
         } else {
-            localStorage.removeItem("dark-mode");
-            document.documentElement.classList.remove("dark-mode");
+            localStorage.setItem("theme", "light");
         }
     });
 });
