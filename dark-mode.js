29150b915fa0fd98dc77b251417891db60b74cde
GalaxyShard <dominic.adragna@byteroach.com>
2025-04-17 21:43:21 -0400

    license javascript code under AGPL-3.0-or-later

--- dark-mode.js
+++ dark-mode.js
@@ -1,3 +1,6 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
+
+// This script is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 let darkMode = localStorage.getItem("dark-mode");
 if (darkMode) {
@@ -19,3 +22,5 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// @license-end
